9a23adca1ba242581557f030ed5f544bea4650a8
i-sandwellrgu <114064741+i-sandwellrgu@users.noreply.github.com>
2024-04-04 17:06:14 +0100

    Update forever.py

--- forever.py
+++ forever.py
@@ -1,6 +1,10 @@
-#!/usr/local/lib/python3.7from subprocess import Popenimport sys
+#!/usr/local/lib/python3.7
+from subprocess import Popen
+import sys
 
-filename = sys.argv[1]while True:
+filename = sys.argv[1]
+
+while True:
     print("\nStarting " + filename)
     p = Popen("python3 " + filename, shell=True)
     p.wait()
